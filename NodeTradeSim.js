dfc2ec0dee47e6ac47a34bd73e44414015894f57
Betwulf <betwulf@hotmail.com>
2015-05-08 13:32:21 -0400

    Updated AppInsights code

--- NodeTradeSim.js
+++ NodeTradeSim.js
@@ -11,7 +11,11 @@ var bodyParser = require('body-parser');
 
 // Setup Application Insights for Azure
 var appInsights = require("applicationinsights");
-appInsights.setup(config.appInsightsKey).setAutoCollectRequests(false).start();
+appInsights.setup(config.appInsightsKey)
+    .setAutoCollectRequests(false)
+    .setAutoCollectPerformance(false)
+    .setAutoCollectExceptions(false)
+    .start();
 appInsights.client.commonProperties = { environment: process.env.TERM };
 
 
@@ -26,7 +30,7 @@ log.info("Written by John Placais, (C) 2015 - More than All Rights Reserved.");
 
 // create express logging function
 var expresslog = function expressLog(req, res, next) {
-    appInsights.client.trackRequest(req, res); // track unhandled
+    appInsights.client.trackRequest(req, res); // track for azure
     log.debug("Request: " + req.url);
     next(); // Passing the request to the next handler in the stack.
 }
@@ -70,12 +74,14 @@ try {
     expressapp.use(express.static(path.join(__dirname, 'Website')));
     expressapp.get('*', function (req, res) {
         appInsights.client.trackRequest(req, res); // track unhandled
+        
         log.warn("unhandled: " + req.url);
     });
     
     // Begin Listening
     appInsights.client.trackEvent("server start");// track system startup event 
 } catch (err) {
+    appInsights.client.trackException(err);
     log.error('Main listen loop crash', err);
 }
 
